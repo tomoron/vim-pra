ba0abb928ddd7670c87f6d3f845e2d03368e98a2
tomoron <tomoron326@outlook.jp>
2022-11-02 20:24:17 +0900

    fix

--- csv.rb
+++ csv.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 csv_string = CSV.generate do |csv|
   csv << ["id","name","text"]
-  [["1","t1","a"],["2","t2","b"],["3","t3","c"]].each do |row|
+  [["1","t1","a"],["2","t2","b"],["3","t3","c"],["4","t4","d"]].each do |row|
     csv << row
   end
 end
